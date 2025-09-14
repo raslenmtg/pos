8468aa613aa1daaeeb4d2c1b19031ff78f019338
Med Raslen <raslen654@gmail.com>
2025-09-14 12:33:52 +0100

    pixel fb

--- resources/views/layouts/auth2.blade.php
+++ resources/views/layouts/auth2.blade.php
@@ -11,17 +11,45 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title') - {{ config('app.name', 'POS') }}</title>
+    <title>@yield('title') - Simplex gestion</title>
 
     @include('layouts.partials.css')
 
     @include('layouts.partials.extracss_auth')
 
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <script src='https://www.google.com/recaptcha/api.js'></script>
+ <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "ta3w6x8bub");
+    </script>
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '24661890816831066');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=24661890816831066&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-M57CQPB9JN"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-M57CQPB9JN');
+    </script>
+    <script src="{{ asset('js/login.js?v=' . $asset_v) }}"></script>
 
 </head>
 
