63065391a9bd368799be2fdbebcfcec4a7e272fe
Med Raslen <raslen654@gmail.com>
2025-12-09 08:55:07 +0100

    remove raleway font

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,7 +11,6 @@
         <title>@yield('title')</title>
 
         <!-- Fonts -->
-        <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,600" rel="stylesheet" type="text/css"> -->
 
         <link rel="stylesheet" href="{{ asset('css/vendor.css') }}">
 
