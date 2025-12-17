1124e902b3d7a2bfef662e01824c32bfa1b037b0
Med Raslen <raslen654@gmail.com>
2025-12-17 08:54:35 +0100

    fix breaking detail invoice

--- resources/views/sale_pos/receipts/detailed.blade.php
+++ resources/views/sale_pos/receipts/detailed.blade.php
@@ -14,7 +14,7 @@
         }
 
         /* Allow page breaks in tables but not in rows */
-        table { page-break-inside: auto; }
+        table { page-break-inside: auto;font-size: 12px; }
         tr { page-break-inside: avoid; page-break-after: auto; }
 
         /* Repeat table headers on each page */
