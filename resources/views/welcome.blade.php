9314eca8d0dc1f9f5cba393dabdadf6b82bed37b
Med Raslen <raslen654@gmail.com>
2026-02-03 08:22:07 +0100

    remove localhost pixel from meta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,7 +80,6 @@
 
 
     <!-- Meta Pixel Code -->
-    @if(!in_array(request()->getHost(), ['localhost', '127.0.0.1']))
     <script>
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -96,7 +95,6 @@
     <noscript><img height="1" width="1" style="display:none"
                    src="https://www.facebook.com/tr?id=768128085832071&ev=PageView&noscript=1"
         /></noscript>
-    @endif
     <!-- End Meta Pixel Code -->
 
     <!-- JSON-LD Structured Data - Organization -->
