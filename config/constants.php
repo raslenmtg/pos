7def4dd0e732df26bac04fdb36c2beb4df83aab8
Med Raslen <raslen654@gmail.com>
2026-03-02 17:28:55 +0100

    inc assset vers

--- config/constants.php
+++ config/constants.php
@@ -20,7 +20,7 @@ return [
     'document_size_limit' => '5000000', //in Bytes,
     'image_size_limit' => '5000000', //in Bytes
 
-    'asset_version' => 2.94,
+    'asset_version' => 2.95,
 
     'disable_purchase_in_other_currency' => true,
 
