b106d4a7b9b71d04b921c61f7dd6e6b7a2d9be05
Med Raslen <raslen654@gmail.com>
2026-09-18 22:01:11 +0100

    Revert "Completely redesign POS screen UI"
    This reverts commit c3a41996c1cb205160a3fb7e89d0b45f4d641b9a.

--- resources/views/sale_pos/partials/product_list.blade.php
+++ resources/views/sale_pos/partials/product_list.blade.php
@@ -1,22 +1,49 @@
 @forelse($products as $product)
-    <div class="col-md-3 col-sm-4 col-xs-6 product_list no-print">
-        <div class="product_box pos-product-card" data-variation_id="{{$product->id}}" title="{{$product->name}} @if($product->type == 'variable')- {{$product->variation}} @endif {{ '(' . $product->sub_sku . ')'}}">
-            <div class="pos-product-image image-container" style="background-image:url(@if(count($product->media) > 0){{$product->media->first()->display_url}}@elseif(!empty($product->product_image)){{asset('/uploads/img/' . rawurlencode($product->product_image))}}@else{{asset('/img/default.png')}}@endif);background-repeat:no-repeat;background-position:center;background-size:contain;"></div>
-            <div class="pos-product-info">
-                <div class="pos-product-name">{{$product->name}} @if($product->type == 'variable')<span>- {{$product->variation}}</span>@endif</div>
-                <div class="pos-product-sku">{{$product->sub_sku}}</div>
-                <div class="pos-product-meta">
-                    @if(!empty($show_prices))
-                        <strong>@format_currency($product->selling_price)</strong>
-                    @endif
-                    @if($product->enable_stock)
-                        <span>{{ @num_format($product->qty_available) }} {{$product->unit}}</span>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="col-md-3 col-xs-4 product_list no-print">
+		<div class="product_box hover:tw-shadow-lg hover:tw-animate-pulse" data-variation_id="{{$product->id}}" title="{{$product->name}} @if($product->type == 'variable')- {{$product->variation}} @endif {{ '(' . $product->sub_sku . ')'}} @if(!empty($show_prices)) @lang('lang_v1.default') - @format_currency($product->selling_price) @foreach($product->group_prices as $group_price) @if(array_key_exists($group_price->price_group_id, $allowed_group_prices)) {{$allowed_group_prices[$group_price->price_group_id]}} - @format_currency($group_price->price_inc_tax) @endif @endforeach @endif">
+
+		<div class="image-container" 
+			style="background-image: url(
+					@if(count($product->media) > 0)
+						{{$product->media->first()->display_url}}
+					@elseif(!empty($product->product_image))
+						{{asset('/uploads/img/' . rawurlencode($product->product_image))}}
+					@else
+						{{asset('/img/default.png')}}
+					@endif
+				);
+			background-repeat: no-repeat; background-position: center;
+			background-size: contain;">
+			
+		</div>
+
+		<div class="text_div">
+			<small class="text text-muted">{{$product->name}} 
+			@if($product->type == 'variable')
+				- {{$product->variation}}
+			@endif
+			</small>
+
+			<small class="text-muted">
+				({{$product->sub_sku}})
+			</small>
+			<!-- <br>
+			<small class="text-muted" style="font-size: 60%;">
+				@if($product->enable_stock)
+				{{ @num_format($product->qty_available) }} {{$product->unit}} @lang('lang_v1.in_stock')
+				@else
+					--
+				@endif
+			</small> -->
+		</div>
+			
+		</div>
+	</div>
 @empty
-    <input type="hidden" id="no_products_found">
-    <div class="col-md-12"><div class="pos-empty-products"><i class="fas fa-box-open"></i><strong>@lang('lang_v1.no_products_to_display')</strong><span>Try another search or category.</span></div></div>
+	<input type="hidden" id="no_products_found">
+	<div class="col-md-12">
+		<h4 class="text-center">
+			@lang('lang_v1.no_products_to_display')
+		</h4>
+	</div>
 @endforelse
